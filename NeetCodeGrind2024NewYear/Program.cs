dfb960eb9eb4253d6987d18b1f2ae54afe90e5d6
Raen Reyes <raen.reyes11@outlook.com>
2024-01-03 15:29:16 -0800

    Add project files.

---
+++ NeetCodeGrind2024NewYear/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NeetCodeGrind2024NewYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello This is the start of the blind 150 solving problems");
+        }
+    }
+}
