937c5a6a3ecd4fc820b5e5c1b5690904741652b1
Raen Reyes <raen.reyes11@outlook.com>
2024-01-03 15:34:54 -0800

    217 contains duplicate. This was not done in O(n) it was O(n^2) . Im gonna learn more about hashtables and lists to get prepared

--- NeetCodeGrind2024NewYear/Program.cs
+++ NeetCodeGrind2024NewYear/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello This is the start of the blind 150 solving problems");
+            Console.WriteLine("217 contains duplicate");
+            int[] nums = { 1, 2, 3, 4 };
+            ContainsDuplicate(nums);
+        }
+        public static bool ContainsDuplicate(int[] nums)
+        {
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++) { 
+                    if (nums[i] == nums[j])
+                    {
+                        Console.WriteLine("True");
+                        return true;
+                    }
+                }
+            }
+            Console.WriteLine("False");
+            return false;
         }
     }
 }
