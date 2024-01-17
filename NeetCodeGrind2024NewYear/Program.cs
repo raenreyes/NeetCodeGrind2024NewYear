afe8af2a44cf69cf70f135f07d09b4f04d499ef7
Raen Reyes <raen.reyes11@outlook.com>
2024-01-16 17:36:57 -0800

    7 reverse integer

--- NeetCodeGrind2024NewYear/Program.cs
+++ NeetCodeGrind2024NewYear/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Text.RegularExpressions;
+using System.Collections.Concurrent;
+using System.Collections;
 
 namespace NeetCodeGrind2024NewYear
 {
@@ -6,24 +8,32 @@ namespace NeetCodeGrind2024NewYear
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] { 1, 2, 3, 1, 2, 3 };
-            
-            Console.WriteLine(ContainsNearbyDuplicate(nums, 2));
-        }
-        public static bool ContainsNearbyDuplicate(int[] nums, int k)
-        {
-            for(int i = 0; i < nums.Length; i++)
-            {
-                for (int j = i + 1; j < nums.Length; j ++)
-                {
-                    if (nums[i] == nums[j] && Math.Abs(i - j) <= k)
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
 
+            Console.WriteLine(Reverse(123));
+        }
+        public static int Reverse(int x)
+        {
+            int result = 0;
+
+            while(x != 0)
+            {
+                int remainer = x % 10;
+                int temp = result * 10 + remainer;
+
+                if ((temp - remainer) / 10 != result) {
+
+                    return 0;
+                }
+
+                result = temp;
+                x = x / 10;
+
+            }
+            return result;
+        }
     }
+
+
 }
+
+
